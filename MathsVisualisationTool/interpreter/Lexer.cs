a151b357ec0e509909c26817f498e43c680faf80
DESKTOP-CI5CUM0\Vincent Nicholson <vince1998@hotmail.co.uk>
2019-10-10 20:54:35 +0100

    Begun Tokanisation

--- MathsVisualisationTool/interpreter/Lexer.cs
+++ MathsVisualisationTool/interpreter/Lexer.cs
@@ -11,19 +11,88 @@ namespace MathsVisualisationTool
 {
     class Lexer
     {
+        //Supported data types by the lexer.
+        private readonly string [] SUPPORTED_DATA_TYPES = new string [] { "INTEGER" };
+        //Supported operations by the lexer.
+        private readonly string [] SUPPORTED_OPS = new string[] { "PLUS", "MINUS", "DIVISION", "MULTIPLICATION" };
+        //Miscellaneous characters.
+        private readonly string[] MISC = new string [] { "EOL" };
+
+        private List<Token> tokens;
+        
         public Lexer()
         {
-
+            tokens = new List<Token>();
         }
 
-        public string tokeniseInput(string input)
+        /*
+         * Function to tokenise the user's input. 
+         */
+        public void tokeniseInput(string input)
         {
-            JToken config = loadInterpreterConfig();
 
-            JToken [] supportedTypes = config["SUPPORTED_TYPES"].ToArray();
+            Token tokenToAdd = new Token();
 
-            Console.WriteLine(supportedTypes[0].ToString());
-            return "yes";
+            //Go through the line of code added by the user.
+            foreach( char c in input)
+            {
+                if(char.IsWhiteSpace(c))
+                {
+                    //if there where values added 
+                    if (!tokenToAdd.isEmpty())
+                    {
+                        tokens.Add(tokenToAdd);
+                        tokenToAdd = new Token();
+                    }
+                }
+
+                //Is it a digit?
+                if(char.IsDigit(c))
+                {
+                    //if the previous token wasn't an integer
+                    if (tokenToAdd.getType() != SUPPORTED_DATA_TYPES[0])
+                    {
+                        tokens.Add(tokenToAdd);
+                    } else if (!tokenToAdd.isEmpty())
+                    {
+                        //the previous token was an integer before so append it
+                        tokenToAdd.appendToValue(c);
+                    } else
+                    {
+                        tokenToAdd = new Token(SUPPORTED_DATA_TYPES[0], char.ToString(c));
+                    }
+                        
+                }
+
+                if(c == '+')
+                {
+                    //if the previous token wasn't an integer
+                    if (tokenToAdd.getType() != SUPPORTED_OPS[0])
+                    {
+                        tokens.Add(tokenToAdd);
+                    }
+                    else if (!tokenToAdd.isEmpty())
+                    {
+                        throw new Exception(" '++' is not a valid expression");
+                    }
+                    else
+                    {
+                        tokenToAdd = new Token(SUPPORTED_OPS[0], char.ToString(c));
+                    }
+                }
+            }
+
+            if(!tokenToAdd.isEmpty())
+            {
+                tokens.Add(tokenToAdd);
+            }
+
+            foreach(Token t in tokens)
+            {
+                Console.WriteLine(t.ToString());
+            }
+
+            Console.WriteLine("--------------------------------------------------");
         }
 
         /*
@@ -49,12 +118,53 @@ namespace MathsVisualisationTool
     class Token
     {
         private string type;
-        private int value;
+        private string value;
 
-        public Token(string type, int value)
+        public Token()
+        {
+            this.type = "";
+            this.value = "";
+        }
+
+        public Token(string type, string value)
         {
             this.type = type;
             this.value = value;
+        }
+
+        public void appendToValue(char valueToAppend)
+        {
+            this.value += valueToAppend;
+        }
+
+        public string getType()
+        {
+            return type;
+        }
+
+        public string getValue()
+        {
+            return value;
+        }
+
+        public void setType(string type)
+        {
+            this.type = type;
+        }
+
+        public void setValue(string value)
+        {
+            this.value = value;
+        }
+
+        public bool isEmpty()
+        {
+            return (value.Length == 0); 
+        }
+
+        public override string ToString()
+        {
+            return "(" + this.type + "," + this.value + ")";
         }
     }
 }
