318d093693e4148ed0023892de2fb05ab8481cd5
vrq16kku <vrq16kku@uea.ac.uk>
2019-10-29 22:31:39 +0000

    extra stuff2

--- Parser/Parser.fs
+++ Parser/Parser.fs
@@ -2,12 +2,15 @@
 // See the 'F# Tutorial' project for more help.
 
 namespace Parser.FSharpImplementation
+open System.Collections.Generic;
 open DataDomain;
-open System.Collections;
 
-module Expression =
-    (*let expression tokens:List<Token> =
-        let term =
+module Expressions =
+
+    let expression tokens:double =
+        2.0+2.0
+
+    let add num1 = 
+        num1+num1
             
-        let expression1 = *) 
     
