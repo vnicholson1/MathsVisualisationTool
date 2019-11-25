fbe30a128dfb9e0b61556ec83a0453760e2ab580
Vincent Nicholson (CMP - Student) <vrq16kku@UEA.AC.UK>
2019-11-25 16:20:12 +0000

    graphng stuff

--- MathsVisualisationTool/common/GraphDrawerController.cs
+++ MathsVisualisationTool/common/GraphDrawerController.cs
@@ -19,6 +19,7 @@ namespace MathsVisualisationTool
     {
 
         private PlotFunction plotFunc;
+        private readonly int MARGIN = 10;
 
         public GraphDrawer(PlotFunction plotFunc)
         {
@@ -29,76 +30,74 @@ namespace MathsVisualisationTool
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             //margin of the canvas
-            const double margin = 10; 
-            double xminCanvas = margin;
+            double xminCanvas = MARGIN;
             //Width of the canvas minus desired margin of canvas.
-            double xmaxCanvas = graphCanvas.Width - margin;
-            double yminCanvas = margin;
-            double ymaxCanvas = graphCanvas.Height - margin;
-
+            double xmaxCanvas = graphCanvas.Width - MARGIN;
+            double yminCanvas = MARGIN;
+            double ymaxCanvas = graphCanvas.Height - MARGIN;
             //defines the number of pixels between each data point.
-            const double step = 10;
+            double step = Math.Floor(480.0/ Convert.ToDouble(plotFunc.dataPoints.Count - 1));
 
             // Make the X axis.
             GeometryGroup xaxis_geom = new GeometryGroup();
+            //Create a straight horizontal line from (0,ymaxCanvas) to (canvas.width,ymaxCanvas) in canvas coordinates.
             xaxis_geom.Children.Add(new LineGeometry(
                 new Point(0, ymaxCanvas), new Point(graphCanvas.Width, ymaxCanvas)));
-            for (double x = xminCanvas + step;
-                x <= graphCanvas.Width - step; x += step)
+
+            //Iterate through and add | markings to this line.
+            for (double x = xminCanvas;
+                x <= graphCanvas.Width; x += step)
             {
+                //Add | per each step.
                 xaxis_geom.Children.Add(new LineGeometry(
-                    new Point(x, ymaxCanvas - margin / 2),
-                    new Point(x, ymaxCanvas + margin / 2)));
+                    new Point(x, ymaxCanvas - MARGIN / 2),
+                    new Point(x, ymaxCanvas + MARGIN / 2)));
             }
 
+            //Then style the X axis.
             Path xaxis_path = new Path();
             xaxis_path.StrokeThickness = 1;
             xaxis_path.Stroke = Brushes.Black;
             xaxis_path.Data = xaxis_geom;
-
+            //Add it to the graphCanvas.
             graphCanvas.Children.Add(xaxis_path);
 
             // Make the Y ayis.
             GeometryGroup yaxis_geom = new GeometryGroup();
+            //Create a vertical line starting from (xminCanvas,0) to (xminCanvas, canvas.height)
             yaxis_geom.Children.Add(new LineGeometry(
                 new Point(xminCanvas, 0), new Point(xminCanvas, graphCanvas.Height)));
-            for (double y = step; y <= graphCanvas.Height - step; y += step)
-            {
-                yaxis_geom.Children.Add(new LineGeometry(
-                    new Point(xminCanvas - margin / 2, y),
-                    new Point(xminCanvas + margin / 2, y)));
-            }
 
+            //Iterate through and add | markings to this line.
+            for (double y = MARGIN; y <= graphCanvas.Height; y += step)
+            {
+                //Add | per each step
+                yaxis_geom.Children.Add(new LineGeometry(
+                    new Point(xminCanvas - MARGIN / 2, y),
+                    new Point(xminCanvas + MARGIN / 2, y)));
+            }
+            //Then style the Y axis
             Path yaxis_path = new Path();
             yaxis_path.StrokeThickness = 1;
             yaxis_path.Stroke = Brushes.Black;
             yaxis_path.Data = yaxis_geom;
-
+            //Add it to the graph canvas
             graphCanvas.Children.Add(yaxis_path);
 
             // Make some data sets.
-            Brush[] brushes = { Brushes.Red, Brushes.Green, Brushes.Blue };
-            Random rand = new Random();
-            for (int data_set = 0; data_set < 3; data_set++)
+            Brush colour = Brushes.Blue;
+            PointCollection points = new PointCollection();
+            foreach (Tuple<double,double> point in plotFunc.dataPoints)
             {
-                int last_y = rand.Next((int)yminCanvas, (int)ymaxCanvas);
-
-                PointCollection points = new PointCollection();
-                for (double x = xminCanvas; x <= xmaxCanvas; x += step)
-                {
-                    last_y = rand.Next(last_y - 10, last_y + 10);
-                    if (last_y < yminCanvas) last_y = (int)yminCanvas;
-                    if (last_y > ymaxCanvas) last_y = (int)ymaxCanvas;
-                    points.Add(new Point(x, last_y));
-                }
-
-                Polyline polyline = new Polyline();
-                polyline.StrokeThickness = 1;
-                polyline.Stroke = brushes[data_set];
-                polyline.Points = points;
-
-                graphCanvas.Children.Add(polyline);
+                points.Add(new Point(point.Item1*step + MARGIN, point.Item2*200));
             }
+
+            Polyline polyline = new Polyline();
+            polyline.StrokeThickness = 1;
+            polyline.Stroke = colour;
+            polyline.Points = points;
+
+            graphCanvas.Children.Add(polyline);
         }
     }
 }
